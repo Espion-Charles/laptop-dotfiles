544331cfe217cbc7877a3925746d8bb5afe46928
Espion-Charles <charlesleclercq12452.0@gmail.com>
2026-01-01 18:31:43 +0100

    hello

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S hyprland kitty noto-fonts dolphin discord steam grim slurp pipewire wireplumber quickshell xdg-desktop-portal-hyprland wofi okular qt6-wayland nvim hyprpaper hyprlock
+sudo pacman -S hyprland kitty noto-fonts dolphin discord steam grim slurp pipewire wireplumber quickshell xdg-desktop-portal-hyprland wofi okular qt6-wayland nvim hyprpaper hyprlock code zsh
 
 mkdir -p ~/Downloads
 
@@ -13,9 +13,11 @@ cd ~
 
 paru -Syu zen-browser-bin
 
-mkdir -p ../../.config
+mkdir -p ~/.config
 
 ln -s ~/laptop-dotfiles/hypr ~/.config
 ln -s ~/laptop-dotfiles~/quickshell ~/.config
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ln -sf ~/laptop-dotfiles/.zshrc ~
