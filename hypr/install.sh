a6814374cac1507190f743f170ebf9c6e6247c2b
Espion-Charles <charlesleclercq12452.0@gmail.com>
2026-01-01 17:18:09 +0100

    trying to add an install script

---
+++ hypr/install.sh
@@ -0,0 +1,19 @@
+sudo pacman -S hyprland kitty noto-fonts dolphin discord steam grim slurp pipewire wireplumber quickshell xdg-desktop-portal-hyprland wofi okular qt6-wayland
+
+mkdir Downloads
+
+cd -p ~/Downloads
+git clone https://aur.archlinux.org/paru.git
+cd paru/
+makepkg -si
+
+paru -Syu zen-browser-bin
+
+
+if [ -d "~/.config/hypr" ] && [ ! -L "~/.config/hypr" ]; then
+    mv ~/.config/hypr ~/.config/hypr.bak
+fi
+
+ln -s ~/laptop-dotfiles/hypr ~/.config
+ln -s ~/laptop-dotfiles/quickshell ~/.config
+
