5b18be790b7b3ab50690a083f9327aab4187818f
Espion-Charles <charlesleclercq12452.0@gmail.com>
2026-01-01 18:00:55 +0100

    changes

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 sudo pacman -S hyprland kitty noto-fonts dolphin discord steam grim slurp pipewire wireplumber quickshell xdg-desktop-portal-hyprland wofi okular qt6-wayland nvim hyprpaper hyprlock
 
-mkdir -p ../Downloads
+mkdir -p ~/Downloads
 
-cd ../Downloads
+cd ~/Downloads
 git clone https://aur.archlinux.org/paru.git
 cd paru/
 makepkg -si
@@ -13,7 +13,7 @@ paru -Syu zen-browser-bin
 
 mkdir -p ../../.config
 
-ln -s ~/laptop-dotfiles/hypr /.config
-ln -s ~/laptop-dotfiles/quickshell /.config
+ln -s ~/laptop-dotfiles/hypr ~/.config
+ln -s ~/laptop-dotfiles~/quickshell ~/.config
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
